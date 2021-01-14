c89a8aa414cf4ab2a4105b552f0ba236c8aa5681
Clinton Graham <ctgraham@pitt.edu>
2021-01-14 16:57:06 -0600

    Cleanup Panopto settings form, iframe rendering, and temporary file for captioning upload.

--- theme/iframe.tpl.php
+++ theme/iframe.tpl.php
@@ -17,12 +17,12 @@
  */
 ?>
 <iframe src="https://pitt.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=<?php print $url?>
-&autoplay=<?php print $autoplay?>
-&offerviewer=<?php print $offer_viewer?>
-&showtitle=<?php print $showtitle?>
-&showbrand=<?php print $showbrand?>
-&start=<?php print $start_at?>
-&interactivity=<?php print $interactivity?>"
-style="width: <?php print $width?>; height: <?php print $height?>; 
-border: 1px solid #464646;
-" allowfullscreen allow="autoplay"></iframe>
+&autoplay=<?php print htmlentities($autoplay)?>
+&offerviewer=<?php print htmlentities($offer_viewer)?>
+&showtitle=<?php print htmlentities($showtitle)?>
+&showbrand=<?php print htmlentities($showbrand)?>
+&start=<?php print htmlentities($start_at)?>
+&interactivity=<?php print htmlentities($interactivity)?>"
+width="<?php print htmlentities($width)?>"
+height="<?php print htmlentities($height)?>"
+style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
