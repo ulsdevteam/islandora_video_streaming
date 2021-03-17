aa40bdafbde738f78b243ff94105342929929e3f
Clinton Graham <ctgraham@pitt.edu>
2021-03-17 16:40:31 -0400

    remove inline styling

--- theme/iframe.tpl.php
+++ theme/iframe.tpl.php
@@ -9,7 +9,6 @@
 <iframe
 class="panopto_video"
 src="https://<?php print $host?>/Panopto/Pages/Embed.aspx?id=<?php print $url?>&autoplay=<?php print htmlentities($autoplay)?>&offerviewer=<?php print htmlentities($offer_viewer)?>&showtitle=<?php print htmlentities($showtitle)?>&showbrand=<?php print htmlentities($showbrand)?>&start=<?php print htmlentities($start_at)?>&interactivity=<?php print htmlentities($interactivity)?>"
-style="border: 1px solid #464646;"
 allowfullscreen="true"
 allow="autoplay">
 </iframe>
