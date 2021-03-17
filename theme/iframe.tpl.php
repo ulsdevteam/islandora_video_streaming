1db7367c54e6d45b5276822cd8d81abd0838f328
Clinton Graham <ctgraham@pitt.edu>
2021-03-17 16:28:49 -0400

    Remove panopto height/width settings; use panopto host from settings

--- theme/iframe.tpl.php
+++ theme/iframe.tpl.php
@@ -7,9 +7,8 @@
  */
 ?>
 <iframe
-src="https://pitt.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=<?php print $url?>&autoplay=<?php print htmlentities($autoplay)?>&offerviewer=<?php print htmlentities($offer_viewer)?>&showtitle=<?php print htmlentities($showtitle)?>&showbrand=<?php print htmlentities($showbrand)?>&start=<?php print htmlentities($start_at)?>&interactivity=<?php print htmlentities($interactivity)?>"
-width="<?php print htmlentities($width)?>"
-height="<?php print htmlentities($height)?>"
+class="panopto_video"
+src="https://<?php print $host?>/Panopto/Pages/Embed.aspx?id=<?php print $url?>&autoplay=<?php print htmlentities($autoplay)?>&offerviewer=<?php print htmlentities($offer_viewer)?>&showtitle=<?php print htmlentities($showtitle)?>&showbrand=<?php print htmlentities($showbrand)?>&start=<?php print htmlentities($start_at)?>&interactivity=<?php print htmlentities($interactivity)?>"
 style="border: 1px solid #464646;"
 allowfullscreen="true"
 allow="autoplay">
